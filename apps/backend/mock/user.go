5eea580cd838f4f74fd93a65c5705d1e01203ec5
Стив <kenethra.74@gmail.com>
2023-07-05 21:06:35 -0600

    feat(backend): done mocks

--- apps/backend/mock/user.go
+++ apps/backend/mock/user.go
@@ -1,15 +1,28 @@
 package mock
 
 import (
-	"context"
-
 	"github.com/karchx/realword-nx/conduit"
 )
 
 type UserService struct {
-  CreateUserFn func(*conduit.User) error
+	CreateUserFn     func(conduit.User) error
+	AuthenticateFn   func() *conduit.User
+	UserByEmailFn    func(string) *conduit.User
+	UserByUsernameFn func(string) (*conduit.User, error)
 }
 
-func (m *UserService) CreateUser(_ context.Context, user *conduit.User) error {
-  return m.CreateUserFn(user)
+func (m *UserService) CreateUser(user conduit.User) error {
+	return m.CreateUserFn(user)
+}
+
+func (m *UserService) Authenticate(email, password string) (*conduit.User, error) {
+	return m.AuthenticateFn(), nil
+}
+
+func (m *UserService) UserByEmail(email string) (*conduit.User, error) {
+	return m.UserByEmailFn(email), nil
+}
+
+func (m *UserService) UserByUsername(username string) (*conduit.User, error) {
+  return m.UserByUsernameFn(username)
 }
