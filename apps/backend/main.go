4086e89e1a1423c2a1a4b7c108fdfe72378687a8
Стив <kenethra.74@gmail.com>
2023-10-02 21:27:20 -0600

    refactor(backend): remove server old

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -31,33 +31,3 @@ func main() {
 		log.Errorf("%v", err)
 	}
 }
-
-/*type config struct {
-	port  string
-	dbURI postgres.UrlDB
-}
-
-func main() {
-	cfg := envConfig()
-
-	db, err := postgres.Open(&cfg.dbURI)
-
-	if err != nil {
-		log.Fatalf("cannot open database: %v", err)
-	}
-	db.AutoMigrate(&conduit.User{}, &conduit.Follow{})
-	log.Info("Migrated")
-
-	srv := server.NewServer(db)
-	log.Fatal(srv.Run(cfg.port))
-}
-
-func envConfig() config {
-	return config{port: "5001", dbURI: postgres.UrlDB{
-		Host:     "0.0.0.0",
-		Port:     "5432",
-		User:     "postgres",
-		Password: "postgres",
-		Dbname:   "realworld",
-	}}
-}*/
