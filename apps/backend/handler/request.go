11d7913643cc8d43a4a90bcf3647160e2577bcdb
Стив <kenethra.74@gmail.com>
2023-10-03 20:11:05 -0600

    feat(backend): add request handler login

--- apps/backend/handler/request.go
+++ apps/backend/handler/request.go
@@ -30,3 +30,22 @@ func (r *userRegisterRequest) bind(c *fiber.Ctx, u *model.User, v *Validator) er
 	u.Password = h
 	return nil
 }
+
+type userLoginRequest struct {
+	User struct {
+		Email    string `json:"email" validate:"required,email"`
+		Password string `json:"password" validate:"required"`
+	} `json:"user"`
+}
+
+func (r *userLoginRequest) bind(c *fiber.Ctx, v *Validator) error {
+	if err := c.BodyParser(r); err != nil {
+		return err
+	}
+
+	if err := v.Validate(r); err != nil {
+		return err
+	}
+
+	return nil
+}
